3dd93488f1bd5ca814a11543967b6433764d8ca2
franpua <72509322+franpua@users.noreply.github.com>
2021-11-17 00:29:20 +0100

    Cambiando un poco el texto

--- uno.first_app/uno.first_app/ContentView.swift
+++ uno.first_app/uno.first_app/ContentView.swift
@@ -9,8 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Text("Hola Mundo")
+            .fontWeight(.bold)
+            //.font(.title)
+            //.font(.body)
+            .font(.system(.largeTitle, design: .rounded))
+            .foregroundColor(.blue)
+            //.font(.custom("Helvetica neue", size: 25))
+        
     }
 }
 
