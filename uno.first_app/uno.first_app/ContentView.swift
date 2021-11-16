04c2a724b1f9feabb81f4fb787f0592f2793b9b1
franpua <72509322+franpua@users.noreply.github.com>
2021-11-17 00:12:46 +0100

    Creación proyecto 1 en Xcode

---
+++ uno.first_app/uno.first_app/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  uno.first_app
+//
+//  Created by Francisco Gonzalez Rodriguez on 17/11/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ uno.first_app/uno.first_app/uno_first_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  uno_first_appApp.swift
+//  uno.first_app
+//
+//  Created by Francisco Gonzalez Rodriguez on 17/11/21.
+//
+
+import SwiftUI
+
+@main
+struct uno_first_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
